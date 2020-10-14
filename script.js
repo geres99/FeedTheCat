21a6bccba7435af53af0beb15bb56876d92c56a4
Marcin Medryk <medryk.kontakt@gmail.com>
2020-10-14 23:12:55 +0200

    3rd

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-var square = document.querySelector(".square")
-
+var div = document.createElement("div")
+div.className = "square"
+document.body.appendChild(div)
+var square1 = document.querySelector(".square")
+for(var i = 0; i < 5; i++) {
+square1.style.top = 50*i + "px"
+}
