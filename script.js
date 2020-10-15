236bc1e150d73c3da89ca70d0599fa9c447fd00a
Marcin Medryk <medryk.kontakt@gmail.com>
2020-10-15 14:04:16 +0200

    5th

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var initialX
+var initialY
+function showCoords(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var coords = "X coords: " + x + ", Y coords: " + y;
+    document.getElementById("dupa12").innerHTML = coords;
+  }
 for(var i = 0; i < 5; i++) {
 var randomColor = Math.floor(Math.random()*16777215).toString(16);
 var div = document.createElement("div")
@@ -7,9 +15,12 @@ div.style.left = Math.floor(Math.random() * 400 + 1) + 550 + "px"
 div.style.backgroundColor = "#" + randomColor
 document.body.appendChild(div)
 }
-function showCoords(event) {
-    var x = event.clientX;
-    var y = event.clientY;
-    var coords = "X coords: " + x + ", Y coords: " + y;
-    document.getElementById("dupa12").innerHTML = coords;
-  }
+for(var i = 0; i < 5; i++) {
+var dragStart = function(e) {
+    dupa12.innerHTML = "Meow"
+    div.style.top = e.clientY + "px"
+    div.style.left = e.clientX + "px"
+    }
+document.querySelector(".square" + i).addEventListener("mousedown",dragStart)
+}
+  
