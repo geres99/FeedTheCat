ca8fc213ebdd05b6deb8eaf4e461aeade7bc4c47
Marcin Medryk <medryk.kontakt@gmail.com>
2020-10-15 12:57:00 +0200

    4th

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-var div = document.createElement("div")
-div.className = "square"
-document.body.appendChild(div)
-var square1 = document.querySelector(".square")
 for(var i = 0; i < 5; i++) {
-square1.style.top = 50*i + "px"
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+var div = document.createElement("div")
+div.className = "square square" + i
+div.style.top = Math.floor(Math.random() * 250 + 1) + 10 + "px"
+div.style.left = Math.floor(Math.random() * 400 + 1) + 550 + "px"
+div.style.backgroundColor = "#" + randomColor
+document.body.appendChild(div)
 }
+function showCoords(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var coords = "X coords: " + x + ", Y coords: " + y;
+    document.getElementById("dupa12").innerHTML = coords;
+  }
